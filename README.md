dc6b146ef651f4b0ea97e3e76122883a3e52c831
José Antonio Matarán Barrio <jamataran@gmail.com>
2021-10-10 12:24:58 +0200

    Enunciado de la aplicion, dependencias y h2

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# acceso-a-datos-ej2
-Acceso a datos: Ejercicio 2
+# SPRING BOOT II
+
+Segundo ejercicio de Acceso a Datos, Spring Boot, JPA.
+
+## Enunciado
+
+_Tomando como partida el código de la aplicación subido por el profesor, queremos hacer lo siguiente_
+
+## Parte 1
+
+Queremos guardar los cines en los que se va a proyectar cada película (no es necesario almacenar la sala ni el horario, simplemente el cine)
+
+Crear la estructura necesaria en la aplicación para almacenar los cines con la siguiente información:
+
+* Nombre del cine
+* Población
+* Código postal
+* Provincia
+* Precio de la entrada
+
+Para ello tendremos que implementar los métodos: GET, POST, PUT y DELETE
+
+## Parte 2
+
+
---
+++ src/main/java/com/cev/prueba/prueba/web/PeliculasController.java
@@ -0,0 +1,74 @@
+package com.cev.prueba.prueba.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cev.prueba.prueba.domain.Pelicula;
+import com.cev.prueba.prueba.service.PeliculasService;
+import com.cev.prueba.prueba.web.error.CustomError;
+
+@RestController
+public class PeliculasController {
+	
+	@Autowired 
+	PeliculasService peliculasService;			
+
+	@GetMapping(path = "/peliculas")
+	List<Pelicula> getPeliculas(@RequestParam(required = false) String titulo, @RequestParam(required = false, name = "puntuacion.min", defaultValue = "2") int puntuacionMinima ){	
+		
+		if (puntuacionMinima<2) {
+			throw new CustomError("La puntuacion no puede ser menor que 2");			
+		}			
+		if( titulo!= null) {						
+			return peliculasService.buscaPorTitulo(titulo);			
+		}else return peliculasService.getPeliculas();		
+	}
+	
+	
+	@GetMapping(path = "/peliculas/{id}")
+	Pelicula getPeliculas(@PathVariable int id){			
+		return peliculasService.getPelicula(id);		
+	}
+	
+	@PostMapping(path = "/peliculas")
+	int altaPelicula(@RequestBody Pelicula pelicula ) {				
+		return peliculasService.add(pelicula);		
+	}
+	
+	@PutMapping(path = "/peliculas/{id}")
+	Pelicula modificaPelicula(@RequestBody Pelicula pelicula,@PathVariable int id) {
+		peliculasService.guarda(id, pelicula);			
+		return pelicula;		
+	}
+	
+	@DeleteMapping(path="/peliculas/{id}")
+	String borraPelicula(@PathVariable int id) {	
+		peliculasService.borra(id);	
+		return("OK");
+	}
+	
+	@GetMapping(path = "/peliculasHeader")
+	ResponseEntity<List<Pelicula>> getPeliculasHeader(){		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("MiHeaderRespuesta", "HeaderRespuesta");
+		
+		return ResponseEntity.ok().headers(headers).body(peliculasService.getPeliculas());
+	}
+	
+	
+	
+
+}
+
